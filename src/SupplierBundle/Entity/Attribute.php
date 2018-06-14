04d879724126492e76dde14fda32cd8ecbf5da83
pawel <p.krotowski@topmall.pl>
2018-06-14 14:58:21 +0200

    eastend

---
+++ src/SupplierBundle/Entity/Attribute.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace SupplierBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Attribute
+ *
+ * @ORM\Table(name="attribute")
+ * @ORM\Entity(repositoryClass="SupplierBundle\Repository\AttributeRepository")
+ */
+class Attribute
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="supplier_name", type="string", length=255)
+     */
+    private $supplierName;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Attribute
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Attribute
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set supplierName
+     *
+     * @param string $supplierName
+     *
+     * @return Attribute
+     */
+    public function setSupplierName($supplierName)
+    {
+        $this->supplierName = $supplierName;
+
+        return $this;
+    }
+
+    /**
+     * Get supplierName
+     *
+     * @return string
+     */
+    public function getSupplierName()
+    {
+        return $this->supplierName;
+    }
+}
+
---
+++ src/SupplierBundle/Entity/ProductAttribute.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace SupplierBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ProductAttribute
+ *
+ * @ORM\Table(name="product_attribute")
+ * @ORM\Entity(repositoryClass="SupplierBundle\Repository\ProductAttributeRepository")
+ */
+class ProductAttribute
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="product_id", type="integer")
+     */
+    private $productId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="attribute_id", type="integer")
+     */
+    private $attributeId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="string", length=255)
+     */
+    private $value;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set productId
+     *
+     * @param integer $productId
+     *
+     * @return ProductAttribute
+     */
+    public function setProductId($productId)
+    {
+        $this->productId = $productId;
+
+        return $this;
+    }
+
+    /**
+     * Get productId
+     *
+     * @return int
+     */
+    public function getProductId()
+    {
+        return $this->productId;
+    }
+
+    /**
+     * Set attributeId
+     *
+     * @param integer $attributeId
+     *
+     * @return ProductAttribute
+     */
+    public function setAttributeId($attributeId)
+    {
+        $this->attributeId = $attributeId;
+
+        return $this;
+    }
+
+    /**
+     * Get attributeId
+     *
+     * @return int
+     */
+    public function getAttributeId()
+    {
+        return $this->attributeId;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     *
+     * @return ProductAttribute
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return ProductAttribute
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
+
